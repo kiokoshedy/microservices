9758b3976108656bf759723eb6bc76282f08cbe8
Shady Kioko <kiokomusembi1@gmail.com>
2021-11-08 12:05:44 +0300

    set up

---
+++ movie_catalog/src/main/java/io/kioko/movie_catalog/MovieCatalogApplication.java
@@ -0,0 +1,13 @@
+package io.kioko.movie_catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCatalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieCatalogApplication.class, args);
+    }
+
+}
